6d8e0185a79b2076bbff4df6f733385471818139
Rafael de Amaral <rafael_ftec@hotmail.com>
2025-09-20 17:22:20 -0300

    Ingestão de dados com PGVector

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,54 @@
 import os
 from dotenv import load_dotenv
 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_core.documents import Document
+
 load_dotenv()
 
+for k in ("OPENAI_API_KEY","DATABASE_URL","PG_VECTOR_COLLECTION_NAME","PDF_PATH","OPENAI_EMBEDDING_MODEL"):
+    if not os.getenv(k):
+        raise RuntimeError(f"A variável de ambiente {k} não está definida")
+
 PDF_PATH = os.getenv("PDF_PATH")
+DATABASE_URL = os.getenv("DATABASE_URL")
+PG_VECTOR_COLLECTION_NAME = os.getenv("PG_VECTOR_COLLECTION_NAME")
+EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL")
 
 def ingest_pdf():
-    pass
+    docs = PyPDFLoader(str(PDF_PATH)).load()
 
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=150, add_start_index=False).split_documents(docs)
+    if not splits:
+        raise SystemExit(0)
+    
+    enriched = [
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
+    ]
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=PG_VECTOR_COLLECTION_NAME,
+        connection=DATABASE_URL,
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
+
+    print("Ingestão concluída com sucesso.")
 
 if __name__ == "__main__":
     ingest_pdf()
